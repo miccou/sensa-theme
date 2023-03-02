fc9376f08574608d58b20e28f83f59c1be93edd7
miccou <30512204+miccou@users.noreply.github.com>
2023-03-03 10:51:14 +1100

    rsync no longer required

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/zsh
 
 curl -LJO --output-dir ~/.oh-my-zsh/themes/ https://raw.githubusercontent.com/miccou/sensa-theme/master/sensa.zsh-theme 
-rsync -r sensa.zsh-theme ~/.oh-my-zsh/themes/
 sed -i '/ZSH_THEME=".*"/d' ~/.zshrc
 echo 'ZSH_THEME="sensa"'>>~/.zshrc
 source ~/.zshrc
