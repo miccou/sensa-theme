fcb757e247d4943d0c251d0689d4a6304038ca9d
miccou <michael.cousins@outlook.com>
2023-03-03 10:46:32 +1100

    install script - download theme and fix theme reference

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/zsh
 
+curl -LJO --output-dir ~/.oh-my-zsh/themes/ https://raw.githubusercontent.com/miccou/sensa-theme/master/sensa.zsh-theme 
 rsync -r sensa.zsh-theme ~/.oh-my-zsh/themes/
-sed -i '/(ZSH_THEME=".*")/d' ~/.zshrc
-echo 'ZSH_THEME="devcontainers"'>>~/.zshrc
+sed -i '/ZSH_THEME=".*"/d' ~/.zshrc
+echo 'ZSH_THEME="sensa"'>>~/.zshrc
 source ~/.zshrc
